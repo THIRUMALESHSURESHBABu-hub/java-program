7e1b37e1f824ad9092fcd2d37dc5175760d0273c
THIRUMALESHSURESHBABu-hub <thirumaleshsureshbabu03@gmail.com>
2024-11-19 10:21:09 +0530

    Add files via upload

---
+++ WhileLoop.java
@@ -0,0 +1,17 @@
+package com.demo2;
+
+public class WhileLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int i=10;
+        while(i<=100) {    // do while -This will execute code block one before changing if the condition is true
+        	             //.Then it repeat the loop as known as condition is true
+        	
+        	System.out.println(i);
+        	i+=10;
+        	
+        }
+	}
+
+}

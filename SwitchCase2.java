745065694df275e865b1c5d69ef91f292b94a85a
THIRUMALESHSURESHBABu-hub <thirumaleshsureshbabu03@gmail.com>
2024-11-21 18:31:30 +0530

    Add files via upload

---
+++ SwitchCase2.java
@@ -0,0 +1,22 @@
+package com.demo2;
+
+import java.util.Scanner;
+
+public class SwitchCase2 {
+	
+	enum course{Java,Ai,ML}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scr= new Scanner(System.in);
+		String s = scr.nextLine();
+
+		
+			
+		
+	  
+		
+
+	}
+
+}

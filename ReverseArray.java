e525b002c412db1766a6846b1df082400032f06f
THIRUMALESHSURESHBABu-hub <thirumaleshsureshbabu03@gmail.com>
2024-12-12 13:36:03 +0530

    Add files via upload

---
+++ ReverseArray.java
@@ -0,0 +1,13 @@
+package com.demo2;
+
+public class ReverseArray {
+
+	public static void main(String[] args) {
+		int []r = {20,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1};
+		
+		for(int i=0; i<20;i++) {
+			System.out.print(r[i]+" ");
+		}
+	}
+
+}
